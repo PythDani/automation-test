eca231c9d4d7b3ad968a8cd08bcee5a64815c2a3
Rafael Farfan <rafaeldanielfarfan@gmail.com>
2025-09-12 19:14:17 -0500

    record video

--- conftest.py
+++ conftest.py
@@ -508,7 +508,7 @@ def pytest_runtest_makereport(item, call):
                     attachment_type=allure.attachment_type.PNG
                 )
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function", autouse=False)
 def record_screen(request):
     """
     A pytest fixture to record the screen during test execution.
